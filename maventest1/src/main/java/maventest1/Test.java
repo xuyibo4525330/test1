9289d680afa38f2f6645c88f746db00555c65f1c
xuyibo4525330 <50284680+xuyibo4525330@users.noreply.github.com>
2019-05-06 09:22:38 +0800

    Update Test.java

--- maventest1/src/main/java/maventest1/Test.java
+++ maventest1/src/main/java/maventest1/Test.java
@@ -1,5 +1,9 @@
 package maventest1;
 
 public class Test {
-
+  public static void main(String[] args) {
+		System.err.println("hello world");
+		System.out.println("你好");
+    System.out.println("你好");
+	}
 }
